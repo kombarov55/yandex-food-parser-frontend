22176845d34a18a91e7796798bb052cd06804745
kombarov <kombarov@itopcase.ru>
2022-12-02 13:56:08 +0300

    график стоимости блюд

--- src/Screens/SearchFood/SearchFoodScreen.jsx
+++ src/Screens/SearchFood/SearchFoodScreen.jsx
@@ -34,8 +34,57 @@ import axios from "axios";
 import Links from "../../Links";
 import Avatar from "@mui/material/Avatar";
 import FoodCompilationItem from "./FoodCompilationItem";
+import {BarChart, Bar, Legend, Tooltip, XAxis, YAxis} from "recharts";
 
 const drawerWidth = 240;
+
+
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
 
 const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
@@ -223,6 +272,19 @@ function DashboardContent() {
                                     </Stack>
                                 </Stack>
                             </Paper>
+
+                            <Title>
+                                График стоимости блюд
+                            </Title>
+                            <BarChart width={1000}
+                                      height={500}
+                                      data={rs["chart_data"]}>
+                                <XAxis dataKey="shop_name" />
+                                <YAxis dataKey={"price"} />
+                                <Tooltip />
+                                <Legend/>
+                                <Bar dataKey="price" fill="#8884d8" name={"Стоимость блюда"} />
+                            </BarChart>
                         </Stack>
                     </Container>
                 </Box>
