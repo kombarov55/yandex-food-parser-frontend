d5b1b0873bef74f6e4274848c1276235fe33a8f6
kombarov <kombarov@itopcase.ru>
2022-12-05 10:26:01 +0300

    удаление из избранного

--- src/Links.js
+++ src/Links.js
@@ -1,5 +1,5 @@
-// const rootUrl = "https://novemis.ru:8000"
-const rootUrl = "http://localhost:8000"
+const rootUrl = "https://novemis.ru:8000"
+// const rootUrl = "http://localhost:8000"
 
 
 export default {
@@ -12,5 +12,7 @@ export default {
     searchFood: (foodName, amount, email) => `${rootUrl}/search_food/${foodName}/${amount}/${email}`,
     addCompilationItem: (foodId, email) => `${rootUrl}/account/${email}/compilation/${foodId}`,
     removeCompilationItem: (foodId, email, name = "Избранное") => `${rootUrl}/account/${email}/compilation/${name}/${foodId}`,
-    getAllCompilations: (email) => `${rootUrl}/account/${email}/compilation`
+    getAllCompilations: (email) => `${rootUrl}/account/${email}/compilation`,
+    moveCompilationItem: (foodId, email, name, prevName) => `${rootUrl}/account/${email}/compilation/${name}/${foodId}/${prevName}`,
+    deleteCompilation: (email, name) => `${rootUrl}/account/${email}/compilation/${name}`,
 }
--- src/Screens/Compilation/CompilationScreen.jsx
+++ src/Screens/Compilation/CompilationScreen.jsx
@@ -3,16 +3,28 @@ import PageWrapper from "../PageWrapper";
 import axios from "axios";
 import Links from "../../Links";
 import CookieUtil from "../../Util/CookieUtil";
-import {Snackbar, Stack} from "@mui/material";
+import {Button, Dialog, DialogTitle, ListItem, ListItemAvatar, Snackbar, Stack, TextField} from "@mui/material";
 import Title from "../../ExampleProjects/Dashboard/Title";
 import Paper from "@mui/material/Paper";
 import FoodItemInCompilation from "./FoodItemInCompilation";
 import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import ListItemText from "@mui/material/ListItemText";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import IconButton from "@mui/material/IconButton";
+import {useFormik} from "formik";
+
 
 export default ({}) => {
     const [payload, setPayload] = useState([])
     const [names, setNames] = useState([])
     const [showDialog, setShowDialog] = useState(false)
+
+    const [addNew, setAddNew] = useState(false)
+
+    const [foodId, setFoodId] = useState()
+    const [prevName, setPrevName] = useState()
+    const [newName, setNewName] = useState("")
 
     const [showSnackbar, setShowSnackbar] = useState(false)
     const [snackbarText, setSnackbarText] = useState("")
@@ -34,8 +46,19 @@ export default ({}) => {
         })
     }
 
-    function openDialog() {
+    function openDialog(foodId, name) {
+        setPrevName(name)
+        setShowDialog(true)
+        setFoodId(foodId)
+    }
 
+    function onCloseDialog(value) {
+        axios.post(Links.moveCompilationItem(foodId, email, value, prevName)).then(rs => {
+            setShowDialog(false)
+            setShowSnackbar(true)
+            setSnackbarText("Добавлено в подборку " + value)
+            reload()
+        })
     }
 
     useEffect(() => reload(), [])
@@ -52,12 +75,22 @@ export default ({}) => {
                                     <Grid item>
                                         <FoodItemInCompilation v={v}
                                                                onRemove={() => deleteItem(v, dto.name)}
-                                                               onMove={() => openDialog()}
+                                                               onMove={() => openDialog(v.id, dto.name)}
                                         />
                                     </Grid>
                                 )}
                             </Grid>
-
+                            { dto.name != "Избранное" &&
+                                <Button onClick={() => {
+                                    axios.delete(Links.deleteCompilation(email, dto.name)).then(rs => {
+                                        setShowSnackbar(true)
+                                        setSnackbarText("Удалено")
+                                        reload()
+                                    })
+                                }}>
+                                    Удалить
+                                </Button>
+                            }
                         </Stack>
                     </Paper>
                 )}
@@ -67,6 +100,39 @@ export default ({}) => {
                       autoHideDuration={600}
                       onClose={() => setShowSnackbar(false)}
             />
+            <Dialog open={showDialog} onClose={() => setShowDialog(false)}>
+                <DialogTitle>В какой список переместить?</DialogTitle>
+                {names.map(name =>
+                    <ListItem button onClick={() => onCloseDialog(name)} key={name}>
+                        <ListItemAvatar>
+                            <Avatar>
+                                {name.substring(0, 1)}
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText>{name}</ListItemText>
+                    </ListItem>
+                )}
+                {addNew ?
+                    <>
+                        <ListItem>
+                            <TextField label={"Название подборки"}
+                                       autoFocus
+                                       value={newName}
+                                       onChange={(e) => setNewName(e.target.value)}
+                            />
+                            <Button onClick={() => onCloseDialog(newName)}>
+                                Сохранить
+                            </Button>
+                        </ListItem>
+                        <ListItem>
+
+                        </ListItem>
+                    </> :
+                    <IconButton onClick={() => setAddNew(true)}>
+                        <AddCircleIcon/>
+                    </IconButton>
+                }
+            </Dialog>
         </PageWrapper>
     </>
 }
